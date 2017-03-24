8d0aa7921bec8513e2a7dbffecbbbdb79d9ed071
Samuel Clements <sdkclements@gmail.com>
2017-03-24 14:16:02 -0400

    got this shit working wooooo

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 
 class App extends Component {
@@ -7,15 +7,77 @@ class App extends Component {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+
+          <h2>Welcome to React Password Validator</h2>
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+            Please Sign In:
         </p>
+          <PassForm />
       </div>
     );
   }
 }
+
+class PassForm extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            email:'',
+            password: '',
+            confirmPass:''
+
+        };
+
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(event) {
+        const email = event.target.email
+        const password = event.target.password
+        const confirmPass = event.target.confirmPass
+        this.setState(
+            {
+                [email]: event.target.value,
+                [password]: event.target.value,
+                [confirmPass]: event.target.value
+            });
+    }
+
+
+    handleSubmit(event) {
+        if (event.target.password.value === event.target.confirmPass.value){
+        alert('Passwords Match');
+        }
+        else {alert('passwords do not match')}
+        event.preventDefault();
+    }
+
+    render() {
+        return (
+            <form onSubmit={this.handleSubmit}>
+                <label>
+                    Email:
+                    <input name='email' type="text" value={this.state.value} onChange={this.handleChange} />
+                </label>
+                <label>
+                    Password:
+                    <input name='password' type="text" value={this.state.value} onChange={this.handleChange} />
+                </label>
+
+                <label>
+                    ConfirmPassword:
+                    <input name='confirmPass' type="text" value={this.state.value} onChange={this.handleChange} />
+                </label>
+
+
+                <input type="submit" value="Submit" />
+            </form>
+        );
+    }
+}
+
 
 export default App;
